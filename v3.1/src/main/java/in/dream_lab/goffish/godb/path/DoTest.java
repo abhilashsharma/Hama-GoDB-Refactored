c2f653743b11daba9387d1337c5a4a6850ec83aa
abhilashsharma <dx.abhilash@gmail.com>
2017-07-04 23:17:02 +0530

    added indexing and parsing

--- v3.1/src/main/java/in/dream_lab/goffish/godb/path/DoTest.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/path/DoTest.java
@@ -1,10 +1,12 @@
 package in.dream_lab.goffish.godb.path;
 
 import java.io.EOFException;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -15,6 +17,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.NumericRangeQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
 
 import in.dream_lab.goffish.api.AbstractSubgraphComputation;
 import in.dream_lab.goffish.api.IEdge;
@@ -23,13 +42,18 @@ import in.dream_lab.goffish.api.IRemoteVertex;
 import in.dream_lab.goffish.api.ISubgraph;
 import in.dream_lab.goffish.api.ISubgraphWrapup;
 import in.dream_lab.goffish.api.IVertex;
+import in.dream_lab.goffish.godb.ConfigFile;
 import in.dream_lab.goffish.godb.EdgeAttr;
 import in.dream_lab.goffish.godb.Hueristics;
 import in.dream_lab.goffish.godb.HueristicsLoad;
 import in.dream_lab.goffish.godb.MapValue;
 import in.dream_lab.goffish.godb.Path;
+import in.dream_lab.goffish.godb.Step;
+import in.dream_lab.goffish.godb.Step.Direction;
+import in.dream_lab.goffish.godb.Step.Type;
 import in.dream_lab.goffish.godb.reach.ReachMessage;
 import in.dream_lab.goffish.godb.path.PathMessage.InEdgesWriter;
+import in.dream_lab.goffish.godb.path.TraversalStep.TraversalWithState;
 
 
 public class DoTest extends
@@ -38,7 +62,17 @@ public class DoTest extends
 
 	public static final Log LOG = LogFactory.getLog(DoTest.class);
 	//Clear this at the end of the query
-	
+	static File vertexIndexDir;
+        static Directory vertexDirectory;
+        static Analyzer analyzer;
+        static IndexReader indexReader;
+        static IndexSearcher indexSearcher;
+        static BooleanQuery query;
+        static ScoreDoc[] hits;
+        static boolean initDone = false ;
+        static boolean queryMade = false;
+        private static final Object initLock = new Object();
+        private static final Object queryLock = new Object();
 	
 	// FIXME: We're copying this to the subgraph state in sstep 0. Is that fine?
 	String queryParam;
@@ -50,8 +84,141 @@ public class DoTest extends
 	 * @param initMsg
 	 */
 	public DoTest(String initMsg) {
-		
+		queryParam=initMsg;
 	}
+
+	
+	/**
+         * Initialize Lucene in memory
+         * searcher = new IndexSearcher (new RAMDirectory (indexDirectory)); 
+         */
+        private void initInMemoryLucene() throws InterruptedException, IOException{
+             {
+               long pseudoPid=getSubgraph().getSubgraphId().get() >> 32;
+                     initDone = true;
+                     vertexIndexDir = new File(ConfigFile.basePath+ "/index/Partition"+pseudoPid+"/vertexIndex");
+                     vertexDirectory = FSDirectory.open(vertexIndexDir);
+                     analyzer = new StandardAnalyzer(Version.LATEST);
+                     indexReader  = DirectoryReader.open(new RAMDirectory(vertexDirectory, IOContext.READ));//passing RAM directory to load indexes in memory
+                     indexSearcher = new IndexSearcher(indexReader);
+             }
+          
+          
+        }
+        
+
+        /**
+         * 
+         * uses index to query vertices/Edges that contain a particular property with specified value
+         */
+        private void makeQuery(String prop,Object val) throws IOException{
+                {
+                        queryMade = true;
+                        if(val.getClass()==String.class){
+                        query  = new BooleanQuery();
+                        query.add(new TermQuery(new Term(prop, (String)val)), BooleanClause.Occur.MUST);
+                        hits =  indexSearcher.search(query,40000).scoreDocs;
+                        }
+                        else if(val.getClass()==Integer.class)
+                        {
+                                Query q = NumericRangeQuery.newIntRange(prop,(Integer)val, (Integer)val, true, true);
+                                hits =  indexSearcher.search(q,40000).scoreDocs;
+                        }
+                        
+                }
+        }
+        
+        /**
+         * Initialize the class variables
+         * This method is called in first superstep, it parses the query passed.
+         * It also reads the Graph statistics(Called as Heuristics) from disk
+         */
+        private void init(){
+                PathState state=getSubgraph().getSubgraphValue();
+                String arguments = queryParam;
+                state.Arguments=queryParam;
+          
+        
+                
+            state.path = new ArrayList<Step>();
+                Type previousStepType = Type.EDGE;
+                for(String _string : arguments.split(Pattern.quote("//"))[0].split(Pattern.quote("@")) ){
+                        if(_string.contains("?")){
+                                if(previousStepType == Type.EDGE)
+                                        state.path.add(new Step(Type.VERTEX,null, null, null));
+                                previousStepType = Type.EDGE;
+                                String[] _contents = _string.split(Pattern.quote("?")); 
+                                String p = null ;
+                                Object v = null ;
+                                Direction d = (_contents[0].equals("out") ) ? Direction.OUT : Direction.IN;
+                                if ( _contents.length > 1 )     {
+                                        p = _contents[1].split(Pattern.quote(":"))[0];
+                                        String typeAndValue = _contents[1].split(Pattern.quote(":"))[1];
+                                        String type = typeAndValue.substring(0, typeAndValue.indexOf("["));
+                                        if(type.equals("float")) {
+                                                v = Float.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+                                        }
+                                        else if(type.equals("double")) { 
+                                                v = Double.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+
+                                        }
+                                        else if(type.equals("int")) { 
+                                                v = Integer.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+
+                                        }
+                                        else { 
+                                                v = String.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+
+                                        }
+                                }
+                                state.path.add(new Step(Type.EDGE, d, p, v));
+                        }
+                        else{
+                                previousStepType = Type.VERTEX;
+                                String p = _string.split(Pattern.quote(":"))[0];
+                                String typeAndValue = _string.split(Pattern.quote(":"))[1];
+                                Object v = null;
+                                String type = typeAndValue.substring(0, typeAndValue.indexOf("["));
+                                if(type.equals("float")) {
+                                        v = Float.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+                                }
+                                else if(type.equals("double")) { 
+                                        v = Double.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+
+                                }
+                                else if(type.equals("int")) { 
+                                        v = Integer.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+
+                                }
+                                else { 
+                                        v = String.valueOf(typeAndValue.substring(typeAndValue.indexOf("[") + 1, typeAndValue.indexOf("]")) );
+
+                                }
+
+                                state.path.add(new Step(Type.VERTEX,null, p, v));
+                        }
+                        
+
+                }
+                if(previousStepType == Type.EDGE){
+                        state.path.add(new Step(Type.VERTEX,null, null, null));
+                }
+                state.noOfSteps = state.path.size();
+                state.queryCostHolder = new Double[state.noOfSteps];
+                for (int i = 0; i < state.queryCostHolder.length; i++) {
+                        state.queryCostHolder[i] = new Double(0);
+                        
+                }
+                state.forwardLocalVertexList = new LinkedList<TraversalWithState>();
+                state.revLocalVertexList = new LinkedList<TraversalWithState>();
+//              inVerticesMap = new HashMap<Long, HashMap<String,LinkedList<Long>>>();
+//              remoteSubgraphMap = new HashMap<Long, Long>();
+//              hueristics=HueristicsLoad.getInstance();//loading this at a different place
+
+                
+        }
+
+
 
 
 	////////////////////////////////////////////////////////////////
@@ -70,7 +237,7 @@ public class DoTest extends
 		PathState state = subgraph.getSubgraphValue();
 
 //		state.rootQuerier = rootQuerier;
-		queryParam="test";
+
 		if (queryParam == null) {
 			throw new RuntimeException("Invalid input query. Found NULL");
 		}
@@ -80,16 +247,17 @@ public class DoTest extends
 		if (LOG.isInfoEnabled()) LOG.info("***************ARGUMENTS************** :" + queryParam);
 //		state.query = new ReachQuery(queryParam);
 
-		// init data structures in state
-		// results for traversed edges for local root vertices
-//		state.results = new HashMap<>();
-
-		// Load index
-//		getSubgraph().getSubgraphId().get();
-//		state.rootQuerier.loadIndex(getSubgraph().getSubgraphId().get());
+		init();
+                // load index
+                try{
+                        synchronized (initLock) {
+                                if ( !initDone )
+                                      initInMemoryLucene();
+                        }
+                }catch(Exception e){e.printStackTrace();}
 		
 		//Load Heuristics
-//		heuristics=HueristicsLoad.getInstance();
+		heuristics=HueristicsLoad.getInstance();
 		
 		//create InEdges
 		if(state.InEdges==null){
