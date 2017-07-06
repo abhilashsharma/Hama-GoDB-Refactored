786300ff73109eff8fb94899a6180d112f040087
abhilashsharma <dx.abhilash@gmail.com>
2017-07-06 11:42:56 +0530

    added lucene query done

--- v3.1/src/main/java/in/dream_lab/goffish/godb/path/DoTest.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/path/DoTest.java
@@ -1023,7 +1023,7 @@ for(Map.Entry<Long,StringBuilder> remoteSubgraphMessage: getSubgraph().getSubgra
                                                         }
                                                 }
                                                 
-                                      System.out.println("Starting Position:" + state.startPos +"  Query min Cost:" + minCost + "   Path Size:" + state.path.size()); 
+                                      System.out.println("Starting Position:" + state.startPos +"  Query min Cost:" + minCost + "   Path Size:" + state.path.size() + "CurrentQuery:" +currentProperty +","+currentValue.toString()); 
 //                                      System.out.println("*******Querying done********:"+hits.length);
                                         
                                                 if(hits.length>0){
