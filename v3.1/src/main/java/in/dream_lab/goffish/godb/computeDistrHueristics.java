1f2b8658e7a6df01831d17f0c73d5ce29deb4246
abhilashsharma <dx.abhilash@gmail.com>
2017-07-28 10:22:40 +0530

    changed avgRemoteDegree Calculation

--- v3.1/src/main/java/in/dream_lab/goffish/godb/computeDistrHueristics.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/computeDistrHueristics.java
@@ -297,18 +297,18 @@ implements ISubgraphWrapup {
 						entry_inside.getValue().probability = new BigDecimal(entry_inside.getValue().numberMatchingPredicate / subgraphHueristics.numVertices).setScale(8, BigDecimal.ROUND_HALF_UP).doubleValue();
 						entry_inside.getValue().avgOutDegree = new BigDecimal(entry_inside.getValue().numOutDegree / entry_inside.getValue().numberMatchingPredicate).setScale(8, BigDecimal.ROUND_HALF_UP).doubleValue();
 						entry_inside.getValue().avgInDegree = new BigDecimal(entry_inside.getValue().numInDegree / entry_inside.getValue().numberMatchingPredicate).setScale(8, BigDecimal.ROUND_HALF_UP).doubleValue();
-						if(entry_inside.getValue().numRemoteOutDegree > 0 ){
-						  System.out
-                                                      .println("Got Positive RemoteDegree:" + entry_inside.getValue().numRemoteOutDegree);
-						  System.out.println("Avg Calculation:" +entry_inside.getValue().numRemoteOutDegree / entry_inside.getValue().numberMatchingPredicate);
-						}
-						if(entry_inside.getValue().numRemoteInDegree > 0 ){
-                                                  System.out
-                                                      .println("Got Positive RemoteDegree:" + entry_inside.getValue().numRemoteInDegree);
-                                                  System.out.println("Avg Calculation:" +entry_inside.getValue().numRemoteInDegree / entry_inside.getValue().numberMatchingPredicate);
-                                                }
-						entry_inside.getValue().avgRemoteOutDegree = new BigDecimal(entry_inside.getValue().numRemoteOutDegree / entry_inside.getValue().numberMatchingPredicate).setScale(8, BigDecimal.ROUND_HALF_UP).doubleValue();
-						entry_inside.getValue().avgRemoteInDegree = new BigDecimal(entry_inside.getValue().numRemoteInDegree / entry_inside.getValue().numberMatchingPredicate).setScale(8, BigDecimal.ROUND_HALF_UP).doubleValue();
+//						if(entry_inside.getValue().numRemoteOutDegree > 0 ){
+//						  System.out
+//                                                      .println("Got Positive RemoteDegree:" + entry_inside.getValue().numRemoteOutDegree);
+//						  System.out.println("Avg Calculation:" +entry_inside.getValue().numRemoteOutDegree / entry_inside.getValue().numberMatchingPredicate);
+//						}
+//						if(entry_inside.getValue().numRemoteInDegree > 0 ){
+//                                                  System.out
+//                                                      .println("Got Positive RemoteDegree:" + entry_inside.getValue().numRemoteInDegree);
+//                                                  System.out.println("Avg Calculation:" +entry_inside.getValue().numRemoteInDegree / entry_inside.getValue().numberMatchingPredicate);
+//                                                }
+						entry_inside.getValue().avgRemoteOutDegree = entry_inside.getValue().numRemoteOutDegree / entry_inside.getValue().numberMatchingPredicate;
+						entry_inside.getValue().avgRemoteInDegree = entry_inside.getValue().numRemoteInDegree / entry_inside.getValue().numberMatchingPredicate;
 
 					}
 				}
