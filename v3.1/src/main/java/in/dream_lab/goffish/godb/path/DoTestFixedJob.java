b6b859378ad00a3d006f97cf2af63d487bdf333d
abhilashsharma <dx.abhilash@gmail.com>
2017-07-29 17:10:04 +0530

    adding Fixed Path Logs

---
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/path/DoTestFixedJob.java
@@ -0,0 +1,77 @@
+package in.dream_lab.goffish.godb.path;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.cli.ParseException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hama.HamaConfiguration;
+import org.apache.hama.bsp.TextInputFormat;
+import org.apache.hama.bsp.TextOutputFormat;
+
+import com.sun.tools.javac.util.List;
+
+import in.dream_lab.goffish.godb.reach.ReachMessage;
+import in.dream_lab.goffish.hama.GraphJob;
+
+import in.dream_lab.goffish.hama.LongMapPartitionSubsetGsonReader;
+
+import in.dream_lab.goffish.hama.NonSplitTextInputFormat;
+
+public class DoTestFixedJob {
+
+	 public static void main(String args[]) throws IOException,InterruptedException, ClassNotFoundException, ParseException
+	  {
+		  HamaConfiguration conf = new HamaConfiguration();
+		  GraphJob job = new GraphJob(conf, DoTestFixedStart.class);
+		  job.setJobName("InEdges + Path Queries");
+		  job.setInputFormat(TextInputFormat.class);
+		  job.setInputKeyClass(LongWritable.class);
+		  job.setInputValueClass(LongWritable.class);
+		  job.setOutputFormat(TextOutputFormat.class);
+		  job.setOutputKeyClass(LongWritable.class);
+		  job.setOutputValueClass(LongWritable.class);
+		  job.setMaxIteration(20);
+		  job.setGraphMessageClass(PathMessage.class);
+		  job.setInputPath(new Path(args[0]));
+		  job.setOutputPath(new Path(args[1]));
+		  job.setInitialInput(readArgsFromFile(args[2]));
+		  job.setSubgraphValueClass(PathStateFixedTest.class);
+		  /* Reader configuration */
+		    job.setInputFormat(NonSplitTextInputFormat.class);
+		    job.setInputReaderClass(LongMapPartitionSubsetGsonReader.class);
+		  
+		  //job.setSubgraphComputeClass(SubgraphComputeReduce.class);
+		  job.waitForCompletion(true);
+	  }
+	 
+	 
+	 static String  readArgsFromFile(String fileName) throws IOException{
+	   String Args="";
+//	   String fileName="/home/abhilash/QueryMultipleStartingPoints/MultiStartingPointsCountryBFSQueries.txt";
+	   FileReader fr = new FileReader(fileName);
+           BufferedReader br = new BufferedReader(fr);
+
+           String sCurrentLine;
+
+           
+
+           while ((sCurrentLine = br.readLine()) != null) {
+               if(Args.equals(""))
+                   Args=sCurrentLine;
+               else
+                   Args=Args+";" + sCurrentLine;
+               
+           }
+           
+           br.close();
+	   return Args;
+	 }
+	
+}
