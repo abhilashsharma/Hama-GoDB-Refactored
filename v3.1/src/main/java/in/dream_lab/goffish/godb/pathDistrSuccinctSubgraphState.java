35a1b8b417ce564f4223307ff28a10c2fe4c8f6d
abhilash <abhilashsharma>
2017-09-07 00:38:55 +0530

    fixed bug

---
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/pathDistrSuccinctSubgraphState.java
@@ -0,0 +1,104 @@
+package in.dream_lab.goffish.godb;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.hadoop.io.Writable;
+
+import in.dream_lab.goffish.godb.pathDistrSuccinctIndex.OutputMessageSteps;
+import in.dream_lab.goffish.godb.pathDistrSuccinctIndex.OutputPathKey;
+import in.dream_lab.goffish.godb.pathDistrSuccinctIndex.Pair;
+import in.dream_lab.goffish.godb.pathDistrSuccinctIndex.RecursivePathKey;
+import in.dream_lab.goffish.godb.pathDistrSuccinctIndex.RecursivePathMaintained;
+import in.dream_lab.goffish.godb.pathDistrSuccinctIndex.VertexMessageSteps;
+
+public class pathDistrSuccinctSubgraphState implements Writable {
+
+	
+	// Variables and DataStructures
+	
+		 PrintWriter writer;
+
+
+		
+		String Arguments=null;
+		//made STATIC so that smallest subgraphID will read the whole heuristic(graph statistics)
+		
+		
+		//logging result collection time
+		long resultCollectionTime=0;
+		
+		//storing query cost for each query plan
+		Double[] queryCostHolder=null;
+		
+		
+		ArrayList<Step> path = null;
+		
+		LinkedList<VertexMessageSteps> forwardRemoteVertexList = new LinkedList<VertexMessageSteps>();
+		LinkedList<VertexMessageSteps> revRemoteVertexList = new LinkedList<VertexMessageSteps>();
+		LinkedList<OutputMessageSteps> outputList = new LinkedList<OutputMessageSteps>();
+
+		HashMap<Long,ResultSet> resultsMap = new HashMap<Long,ResultSet>();
+			
+		LinkedList<VertexMessageSteps> forwardLocalVertexList;
+		LinkedList<VertexMessageSteps> revLocalVertexList;
+
+		Integer noOfSteps = null;
+		
+		//TODO: change to relative path
+		String heuristicsBasePath = ConfigFile.basePath+"heuristics/hue_";
+		
+		final Base64 base64 = new Base64();
+		
+		int startPos  = 0;
+		
+		Double networkCoeff = new Double(0.116);
+
+
+		//Data Structure for storing inedges 
+		HashMap<Long,HashMap<Long,EdgeAttr>>  InEdges = null;
+
+		/**
+		 * HashMap for recursive path Maintenance
+		 * Used when getting a remote message from  another subgraph say 'S', to record information about endVertex of 'S' so that if any
+		 * output is created then the partial path generated in current subgraph has to be sent back to 'S' for merging.   
+		 */
+		HashMap<OutputPathKey,List<Pair>> outputPathMaintainance = new HashMap<OutputPathKey,List<Pair>>();
+		
+		/**
+		 * For storing partial paths, <queryID,step,direction,endVertex> where endVertex is the vertex in the path that has remote edges.
+		 * 	Used when sending messages to another subgraph for path traversal. It stores the partial path generated in this data structure and sends
+		 *  message to remote subgraph to continue traversal.
+		 */
+		
+		HashMap<RecursivePathKey, List<RecursivePathMaintained>> recursivePaths = new HashMap<RecursivePathKey, List<RecursivePathMaintained>>();
+	
+		/**
+		 *  Used while creating Inedges.
+		 */
+		HashMap<Long,StringBuilder> MessagePacking=new HashMap<Long,StringBuilder>();
+		
+		//For Storing partial results
+		HashMap<RecursivePathKey,List<String>> partialResultCache=new HashMap<RecursivePathKey,List<String>>();
+
+		
+	@Override
+	public void readFields(DataInput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void write(DataOutput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+}
--- v3.1/src/main/java/in/dream_lab/goffish/godb/succinct/SuccinctSubgraph.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/succinct/SuccinctSubgraph.java
@@ -17,13 +17,16 @@ public class SuccinctSubgraph {
     }
     public List<Long> getVertices(String name, String value)
     {
+    	System.out.println("Calling getVertices");
     	List<Long> vid = new ArrayList<>();
     	int offset;
     	Integer[] recordID = succinctIndexedFileBuffer.recordSearchIds(value.getBytes());
+    	System.out.println("RecordId returned:"+ recordID[0]);
     	for (Integer rid : recordID)
     	{
     		offset = succinctIndexedFileBuffer.getRecordOffset(rid); 
     		vid.add(Long.parseLong(succinctIndexedFileBuffer.extractUntil(offset, ',')));
+    		System.out.println("Offset:" + offset);
     	}
     	return vid;
     }
