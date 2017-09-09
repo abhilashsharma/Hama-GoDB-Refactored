068bffa18fc6d0c904ec5369e2519c9f31638d9f
abhilash <abhilashsharma>
2017-09-09 16:28:48 +0530

    Changed Succinct Data format

--- v3.1/src/main/java/in/dream_lab/goffish/godb/SuccintDataFormat.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/SuccintDataFormat.java
@@ -18,9 +18,15 @@
  */
 package in.dream_lab.goffish.godb;
 
+
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -48,25 +54,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hama.HamaConfiguration;
 import org.apache.hama.bsp.TextInputFormat;
 import org.apache.hama.bsp.TextOutputFormat;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.NumericRangeQuery;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.IOContext;
-import org.apache.lucene.store.IndexOutput;
-import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.util.Version;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.util.Collection;
@@ -108,32 +95,77 @@ AbstractSubgraphComputation<BFSDistrPropSubgraphState, MapValue, MapValue, Text,
   	//System.out.println("**********SUPERSTEPS***********:" + getSuperstep() +"Message List Size:" + messages.size());
 		
 		
+	  
 		// STATIC ONE TIME PROCESSES
 		{
 			// LOAD QUERY AND INITIALIZE LUCENE
 			int count=0;
-			int ecount=0;
+			int localecount=0;
 			if(getSuperstep() == 0){
+				
+				
+				try  {
+					Writer vertexWriter = new BufferedWriter(new OutputStreamWriter(
+				              new FileOutputStream("/home/abhilash/SuccinctSubgraphFiles/Sub"+getSubgraph().getSubgraphId() + "VertexData" ), "utf-8"));
+					Writer edgeWriter = new BufferedWriter(new OutputStreamWriter(
+				              new FileOutputStream("/home/abhilash/SuccinctSubgraphFiles/Sub"+getSubgraph().getSubgraphId() + "edgeData" ), "utf-8"));
+					
+					
 				for(IVertex<MapValue, MapValue, LongWritable, LongWritable> v : getSubgraph().getLocalVertices()) {
 //					count++;
 //					if(count==1000) {
 //						break;
 //					}
-					LOG.info("VERTEXDATA:" + getSubgraph().getSubgraphId().toString()+"#" + v.getValue().get("patid") + "@" + v.getValue().get("country") + "$" + v.getValue().get("nclass") + "|" );
-					StringBuilder str=new StringBuilder();
-					ecount=0;
-					
+					vertexWriter.write(getSubgraph().getSubgraphId().toString()+"#" + v.getValue().get("patid") + "@" + v.getValue().get("country") + "$" + v.getValue().get("nclass") + "|" );
+					localecount=0;
+//					ArrayList<Long> localSinkArray = new ArrayList<>();
+					ArrayList<Long> remoteSinkArray = new ArrayList<>();
+					StringBuilder str = new StringBuilder();
 					for(IEdge<MapValue, LongWritable, LongWritable> e:v.getOutEdges()) {
 						
-						String sink= ""+e.getSinkVertexId();
-						str.append((ecount==0)?"":":");
-						str.append(sink);
-						System.out.println("STR:" + str.toString());
-						ecount++;
+			
+//						System.out.println("STR:" + str.toString());
+						if(!getSubgraph().getVertexById(e.getSinkVertexId()).isRemote()) {
+							if(localecount==0) {
+								str.append(e.getSinkVertexId().get());
+							}
+							else {
+								str.append(":").append(e.getSinkVertexId().get());
+							}
+							localecount++;
+//							localSinkArray.add(e.getSinkVertexId().get());
+						}
+						else
+						{
+							remoteSinkArray.add(e.getSinkVertexId().get());
+						}
 					}
-					System.out.println("ECOUNT:" + ecount);
-					System.out.println("EDGEDATA:"+ getSubgraph().getSubgraphId().toString()+"#" + v.getVertexId() + "@" + ecount + "%" + str.toString() + "|" );
+					
+					StringBuilder remoteStr = new StringBuilder();
+					int rcount=0;
+					for(long rsink:remoteSinkArray) {
+						if(rcount==0) {
+							remoteStr.append(rsink);
+						}else {
+							remoteStr.append(":").append(rsink);
+						}
+						rcount++;
+					}
+					
+					if(localecount==0) {
+						edgeWriter.write(getSubgraph().getSubgraphId().toString()+"#" + v.getVertexId() + "@" + localecount + "%" + remoteStr.toString() + "|" );
+					}else {
+						if(rcount==0) {
+							edgeWriter.write(getSubgraph().getSubgraphId().toString()+"#" + v.getVertexId() + "@" + localecount + "%" + str.toString() + "|" );
+						}
+						else {
+							edgeWriter.write(getSubgraph().getSubgraphId().toString()+"#" + v.getVertexId() + "@" + localecount + "%" +str.toString()+":"+ remoteStr.toString() + "|" );
+						}
+					}
 				}
+			}catch(Exception e) {
+				
+			}
 			}
 	
 		}
