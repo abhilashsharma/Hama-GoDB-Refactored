ab48c6925deb740d4835bbe10357d2ef4bea89f2
abhilash <abhilashsharma>
2017-09-28 21:55:55 +0530

    adding BFS query generation code for succinct

---
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/BFSDistrPropSuccinctSubgraphState.java
@@ -0,0 +1,76 @@
+package in.dream_lab.goffish.godb;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.hadoop.io.Writable;
+
+import in.dream_lab.goffish.godb.BFSDistrSuccinctProp.ResultSet;
+import in.dream_lab.goffish.godb.BFSDistrSuccinctProp.Step;
+import in.dream_lab.goffish.godb.BFSDistrSuccinctProp.VertexMessageSteps;
+
+public class BFSDistrPropSuccinctSubgraphState implements Writable  {
+
+	PrintWriter writer;
+
+	Integer searchInstanceStart = null;
+
+	Integer searchInstanceEnd = null;
+
+	//private ISubgraphInstance subgraphInstance = null;
+	ArrayList<Step> path = null;
+	
+	LinkedList<VertexMessageSteps> forwardRemoteVertexList = new LinkedList<VertexMessageSteps>();
+	//private LinkedList<VertexMessageSteps> revRemoteVertexList = new LinkedList<VertexMessageSteps>();
+
+	HashMap<Long,ResultSet> resultsMap = new HashMap<Long,ResultSet>();
+		
+	LinkedList<VertexMessageSteps> forwardLocalVertexList;
+	//private LinkedList<VertexMessageSteps> revLocalVertexList;
+	//private HashMap<Long,HashMap<String,LinkedList<Long>>> inVerticesMap;
+	HashMap<Long,Long> remoteSubgraphMap; 
+
+	Integer noOfSteps = null;
+	
+	String heuristicsBasePath = ConfigFile.basePath+"heuristics/hue_";
+	
+	final Base64 base64 = new Base64();
+	
+	int startPos  = 0;
+	
+	double networkCoeff = 0.116;
+
+	//for BFS
+	
+	int Depth;
+	List<Long> visitedVertices=new ArrayList<Long>();
+
+	
+	Long TotalTime=0l;
+	 
+	
+	String Arguments=null;
+	private Double[] queryCostHolder;
+	
+	long resultCollectionTime=0;
+	
+	@Override
+	public void readFields(DataInput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void write(DataOutput arg0) throws IOException {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+}
