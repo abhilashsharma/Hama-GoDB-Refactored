570beca569d2c8c1fb293570b9e95436ccc976ae
abhilash <abhilashsharma>
2017-12-09 21:40:46 +0530

    debugging

--- v3.1/src/main/java/in/dream_lab/goffish/hama/succinctstructure/SuccinctArraySubgraph.java
+++ v3.1/src/main/java/in/dream_lab/goffish/hama/succinctstructure/SuccinctArraySubgraph.java
@@ -143,7 +143,7 @@ public class SuccinctArraySubgraph<S extends Writable, V extends Writable, E ext
     	String record;
     	String[] tokens;
     	long startFine = System.nanoTime();
-    	Integer[] recordID = succinctIndexedVertexFileBuffer.recordSearchIds(value.getBytes());
+    	Integer[] recordID = succinctIndexedVertexFileBuffer.recordSearchIds(("#"+value+"@").getBytes());
     	Log.info("Lookup record id(vertex): "+ (System.nanoTime() - startFine)+ " ns " + recordID.length);
     	for (Integer rid : recordID)
     	 {
