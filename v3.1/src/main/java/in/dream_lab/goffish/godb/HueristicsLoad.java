3d4c4a4f7f51c4a8ad7f002d92706831ad70a855
abhilash <abhilashsharma>
2018-01-29 04:32:31 +0530

    added reading heuristic on argument,,, debugged

--- v3.1/src/main/java/in/dream_lab/goffish/godb/HueristicsLoad.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/HueristicsLoad.java
@@ -50,10 +50,47 @@ public class HueristicsLoad {
 			 
 			 return hueristics;
 		   }
-		 }
-		 
-		
-		 
+		 }	 
 	 }
+	 
+	 
+	 public static Hueristics getInstance(String heuPath){
+		   //add callback later....broadcast to all threads
+		   synchronized(Lock){
+			 if(hueristics!=null){
+			   return hueristics;
+			 }
+			 else{
+			   
+				 try{/*Directly reading the gathered heuristics*/
+					 //TODO: might have to change this when subgraphs run in parallel in a partition(Locking)
+//						FileInputStream fis = new FileInputStream(ConfigFile.basePath +"Hue_FULL.ser"); 
+//						ObjectInputStream ois = new ObjectInputStream(fis);
+//						hueristics = (Hueristics)ois.readObject();
+//						ois.close();
+						
+						// Creating FileSystem object, to be able to work with HDFS
+				                 //hdfs://10.0.0.4:29000/
+				                 //hdfs://orion-00:29000/
+		     			Configuration config = new Configuration();
+		     			config.set("fs.default.name","hdfs://192.168.0.10:29000");
+		     			FileSystem dfs = FileSystem.get(config);
+		     			FSDataInputStream in = null;
+		     			//Exact statistics:/Stats/hue_FULL.ser
+		     			//Approximate statistics://Stats/hue_FULLHist.ser
+		     			in = dfs.open(new Path(heuPath));
+//		     			byte[] b=null;
+//		     			in.readFully(b);	
+		     			ObjectInputStream ois= new ObjectInputStream(in);
+						hueristics=(Hueristics)ois.readObject();
+						System.out.println("Hueristics Loaded");
+						
+					}catch(Exception e){e.printStackTrace();}
+				 
+				 
+				 return hueristics;
+			   }
+			 }	 
+		 }
 	
 }
