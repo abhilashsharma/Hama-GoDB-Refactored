079a9c9a16b10c1de97ba0208f73cbdb66553617
Abhilash <dx.abhilash@gmail.com>
2018-05-03 02:13:03 +0530

    added loggin

--- v3.1/src/main/java/in/dream_lab/goffish/godb/pathDistrTraversalStepPos0.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/pathDistrTraversalStepPos0.java
@@ -1093,7 +1093,7 @@ implements ISubgraphWrapup{
 					/* filtered vertex*/
 					else {
 //						ISubgraphObjectProperties subgraphProperties = subgraphInstance.getPropertiesForVertex(currentVertex.getId());
-						System.out.println("CurrentVertex:" + currentVertex);
+						System.out.println("CurrentVertex:" + currentVertex + " Value:" + currentVertex.getValue() + " nextStep:" + nextStep);
 						String val=currentVertex.getValue().get(nextStep.property);
 						
 						if(val==null) {
