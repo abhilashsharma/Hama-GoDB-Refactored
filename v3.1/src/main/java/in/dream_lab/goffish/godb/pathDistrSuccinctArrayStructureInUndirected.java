84b46b675ffca7a5b9ccbbd191d9fa7c3817bd00
abhilash <abhilash@localhost.localdomain>
2018-05-04 22:47:02 +0530

    changed heuristics to 16P

--- v3.1/src/main/java/in/dream_lab/goffish/godb/pathDistrSuccinctArrayStructureInUndirected.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/pathDistrSuccinctArrayStructureInUndirected.java
@@ -603,7 +603,7 @@ implements ISubgraphWrapup{
 					}catch(Exception e){e.printStackTrace();}
 					
 					LOG.info("Starting loading of heuristics");
-					hueristics=HueristicsLoad.getInstance("/user/abhilash/gplus2PHeuSer/gplus2PHeuristics.ser");//TODO:change heuristics path for new Graph
+					hueristics=HueristicsLoad.getInstance("/user/abhilash/gplusNew16PHeuristics/gplusNew16PHeuristics.ser");//TODO:change heuristics path for new Graph
 					LOG.info("Heuristic Loaded");
 					
 				}
