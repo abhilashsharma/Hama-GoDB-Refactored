24f9f4d3e6c99d62bd00f2c3734438c991561c59
abhilash <dx.abhilash@gmail.com>
2018-10-08 14:32:39 +0530

    disabled heuristics for reachability

--- v3.1/src/main/java/in/dream_lab/goffish/godb/reachabilityDistrSuccinctImplicit.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/reachabilityDistrSuccinctImplicit.java
@@ -357,6 +357,8 @@ implements ISubgraphWrapup{
 				LOG.info("Starting Query Execution");
 				 queryEnd=false;
 			  }
+			  
+			  if(false){
 				//###################################COMPUTE HUERISTIC BASED QUERY COST#################################
 //				{	 
 //					//###########################forward cost#######################################################
@@ -417,7 +419,7 @@ implements ISubgraphWrapup{
 //					//###########################reverse cost#######################################################
 //				}		
 //				//###################################COMPUTE HUERISTIC BASED QUERY COST#################################	
-				
+			  }
 				
 			
 				
@@ -434,7 +436,7 @@ implements ISubgraphWrapup{
 							startPos = 0;
 						else
 							startPos = getSubgraph().getSubgraphValue().queryCostHolder[0]<getSubgraph().getSubgraphValue().queryCostHolder[1] ? 0 : 1;
-//					startPos=0;//used for debugging
+					startPos=0;//used for debugging
 					String currentProperty = null;
 					Object currentValue = null;
 					if (startPos == 0) {
