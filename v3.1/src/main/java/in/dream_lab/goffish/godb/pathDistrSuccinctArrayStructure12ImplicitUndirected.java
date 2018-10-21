95c765720dae10e64adde77310c75a844c249d69
abhilash <dx.abhilash@gmail.com>
2018-10-21 17:06:43 +0530

    changed Heuristics that is loaded

--- v3.1/src/main/java/in/dream_lab/goffish/godb/pathDistrSuccinctArrayStructure12ImplicitUndirected.java
+++ v3.1/src/main/java/in/dream_lab/goffish/godb/pathDistrSuccinctArrayStructure12ImplicitUndirected.java
@@ -584,7 +584,7 @@ implements ISubgraphWrapup{
 					}catch(Exception e){e.printStackTrace();}
 					
 					LOG.info("Starting loading of heuristics");
-					hueristics=HueristicsLoad.getInstance("/user/abhilash/gplusNew2PHeuristics/gplusNew2PHeuristics.ser");//TODO:change heuristics path for new Graph
+					hueristics=HueristicsLoad.getInstance("/user/abhilash/RGraph4PHeuristics/RGraph4PHeu.ser");//TODO:change heuristics path for new Graph
 					LOG.info("Heuristic Loaded");
 					
 				}
